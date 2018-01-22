ca1565b166ecb0426bedecd2d2343de80b988cd1
D-Rod <danrodret@outlook.com>
2018-01-22 09:54:52 -0600

    Hola Mundo, en tres lenguajes

--- src/hola_world/Hola_World.java
+++ src/hola_world/Hola_World.java
@@ -5,9 +5,11 @@ public class Hola_World {
     public static void main(String[] args) {
         String x = "Hola Mundo";
         String y = "Hello World";
+        String v = "Bonjour tout le monde";
 
         System.out.println(x);
-        System.out.println("Hello World");
+        System.out.println(y);
+        System.out.println(v);
 
     }
 }
