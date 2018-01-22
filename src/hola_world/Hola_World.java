d87b034a5f87bbdf98f6e6a3c7bca6ee3b6a8832
D-Rod <danrodret@outlook.com>
2018-01-22 09:49:01 -0600

    Hola Mundo y Hello World

--- src/hola_world/Hola_World.java
+++ src/hola_world/Hola_World.java
@@ -1,12 +1,13 @@
-
 package hola_world;
 
 public class Hola_World {
 
     public static void main(String[] args) {
-       String x = "Hola Mundo";
-       
+        String x = "Hola Mundo";
+        String y = "Hello World";
+
         System.out.println(x);
+        System.out.println("Hello World");
+
     }
-    
 }
