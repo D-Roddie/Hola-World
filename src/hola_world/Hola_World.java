5fdde20261109d44d00b644d41e1f95766e192ca
D-Rod <danrodret@outlook.com>
2018-01-22 09:40:30 -0600

    Primer commit: Hola Mundo

---
+++ src/hola_world/Hola_World.java
@@ -0,0 +1,12 @@
+
+package hola_world;
+
+public class Hola_World {
+
+    public static void main(String[] args) {
+       String x = "Hola Mundo";
+       
+        System.out.println(x);
+    }
+    
+}
